a5715ca463c1bfcefd98096f1715d6d8a45804a4
Tomas Osorio <tomas.osorio@sansano.usm.cl>
2025-09-04 19:31:33 -0400

    fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './dasda.css'
 import Router from './router.tsx';
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
